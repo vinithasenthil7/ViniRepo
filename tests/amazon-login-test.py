2fbe12c2caca531b11b3e98610742da59330c32b
vinithasenthil7 <vinithasenthil7@gmail.com>
2024-08-10 20:25:41 +0530

    Update amazon-login-test.py

--- tests/amazon-login-test.py
+++ tests/amazon-login-test.py
@@ -7,18 +7,16 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 # The webdriver management will be handled by the browserstack-sdk
-# so this will be overridden and tests will run browserstack -
-# without any changes to the test files!
+# so tests will run browserstack without any changes to the test files!
+
 options = ChromeOptions()
 options.set_capability('sessionName', 'Amazon Login Test')
-
 
 driver = webdriver.Remote(
     command_executor='http://localhost:4444/wd/hub',
     options=options)
-driver.implicitly_wait(3)
-driver.maximize_window()
 
+driver.maximize_window()
 
 try:
 
@@ -26,19 +24,19 @@ try:
     driver.get("https://www.amazon.in/")
 
     # Click the "Account & Lists" button
-
     account_button = driver.find_element(By.ID, "nav-link-accountList-nav-line-1")
     account_button.click()
 
-    # Fill in the username/email address
-
+    # Wait until the username page loads
     wait = WebDriverWait(driver, 10)
     continue_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//input[@type= 'submit']")))
 
+    # Fill in the username/email id
     email_field = driver.find_element(By.XPATH, "//input[@name= 'email']")
     email_field.send_keys("9655893313")
     email_field.send_keys(Keys.RETURN)
 
+    # Wait until the password page loads
     wait = WebDriverWait(driver, 10)
     login_button = wait.until(EC.element_to_be_clickable((By.ID, 'signInSubmit')))
 
@@ -47,16 +45,17 @@ try:
     password_field.send_keys("123456")
     password_field.send_keys(Keys.RETURN)
 
+    # Wait until the home page loads
     actual_welcome_message = driver.find_element(By.XPATH, "//*[@id='nav-link-accountList-nav-line-1']").text
     print(actual_welcome_message)
 
+    # Fetch the welcome message in the home page and validate
     expected_welcome_message = "Hello, Vinitha"
     assert expected_welcome_message == actual_welcome_message
-
     print("Login validation passed")
 
 except Exception as e:
-    print("Login validation failed" + str(e))
+    print("Login validation failed " + str(e))
 
 finally:
     # Close the browser
